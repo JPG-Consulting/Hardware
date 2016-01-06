e053c4fcccf8da0ff9a3aa03acc6c16f8b6aeb5b
TuringMacLee <turingmac@hotmail.com>
2016-01-06 20:40:32 +0800

    Relay get

--- Relay/com_relay.py
+++ Relay/com_relay.py
@@ -2,17 +2,35 @@ import serial
 import sys
 import random
 import time
+import json
+import urllib
+import urllib2
+
+apiurldoor = "http://192.168.199.187/command/relaydoor"
+apiurlshelf = "http://192.168.199.187/command/relayshelf"
+data = {}
+
+status = 0
+
+def get(url):
+    #data = urllib.urlencode(data)
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    res = response.read()
+    return res
+
 while True:
     try:
         ser = serial.Serial('COM4', 9600)
     except Exception, e:
         print 'open serial failed.'
         exit(1)
-    #str_control = raw_input('enter control code:')
-    ser.write("%d" % random.randint(0,1))
+    command = get(apiurldoor)
+    #ser.write("%d" % random.randint(0,1))
+    command = command.split(':')
+    ser.write(command[2][0])
     sys.stdout.flush()
     ser.close()
-    time.sleep(0.2)
 
 #input: random number of {0, 1}
 #0:break
