1d16783547f2a3a5e84955af66f62bf077633878
TuringMacLee <turingmac@hotmail.com>
2016-01-06 17:50:44 +0800

    TH post

--- Temperature&Humidity/com_temp&hum.py
+++ Temperature&Humidity/com_temp&hum.py
@@ -1,18 +1,37 @@
 import serial
 import sys
+import urllib
+import urllib2
+import time
+import json
+
+apiurl = "http://192.168.199.187/sensordata/th"
+data = {}
+
+def post(url, data):
+    #data = urllib.urlencode(data)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    res = response.read()
+    return res
+
 while True:
+    str = ""
+    data = {}
     try:
         ser = serial.Serial('COM4', 9600)
     except Exception, e:
         print 'open serial failed.'
         exit(1)
     str = ser.readline()
-    temp = str.split()[1]
-    temp += '\t'
+    data['temperature'] = float(str.split()[1])
     str = ser.readline()
-    temp += str.split()[1]
-    print temp
+    data['humidity'] = float(str.split()[1])
+    data = json.dumps(data)
+    print data
+    print post(apiurl, data)
+
     sys.stdout.flush()
     ser.close()
-
+    time.sleep(1)
 #output: 2-tuple (temperture, humidity)
